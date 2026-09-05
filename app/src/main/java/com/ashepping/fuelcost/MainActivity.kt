57460db148795aae1c6c3c549303564fcb5ab368
ashepping <shepping@yahoo.com>
2026-09-05 13:25:50 +0300

    Theme now switches inside the screen

--- app/src/main/java/com/ashepping/fuelcost/MainActivity.kt
+++ app/src/main/java/com/ashepping/fuelcost/MainActivity.kt
@@ -5,14 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.ashepping.fuelcost.ui.FuelScreen
-import com.ashepping.fuelcost.ui.FuelTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            FuelTheme { FuelScreen() }
-        }
+        setContent { FuelScreen() }
     }
 }
